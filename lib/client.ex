4938f3d040680bcfe012dc8f26eb7c90ba83f0fb
blackthorn39 <blackthorn_39@live.in>
2017-11-23 18:04:13 -0500

    skeleton functionality

--- lib/client.ex
+++ lib/client.ex
@@ -1,5 +1,41 @@
 defmodule Client do
     use GenServer
+    def start_link({x}) do
+        input_srt = Integer.to_string(x)
+        GenServer.start_link(__MODULE__, {x}, name: String.to_atom("user#{x}"))    
+    end
 
-    
+    def init({x}) do        
+       # register self
+        {:ok, {x}}
+    end
+
+    def handle_cast({:register})do
+        #Send register request to server
+        GenServer.call(server,{:registeruser,x})
+        {:noreply,{x}}
+    end
+    def handle_cast({:activate, subscribe_to})do
+        #Subcribe to users
+        GenServer.cast(server,{:subscribe,x,subscribe_to})
+        #Randomly start tweeting/retweeting/subscribe/querying activities acc to zipf rank
+        
+    end
+    def handle_cast(:deactivate})do
+        #stop all activities, play dead
+    end
+
+    def tweet() do
+        #Generate a message
+        msg = "160 random characters"
+        GenServer.cast(server,{:tweet,x,msg})
+    end
+    def subscribe() do
+        #Pick random user
+        subscribe_to = "user1"
+        GenServer.cast(server,{:subscribe,x,subscribe_to})
+    end
+    def query() do
+        
+    end
 end
