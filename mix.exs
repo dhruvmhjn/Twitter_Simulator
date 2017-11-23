afcd85e65e325fc3fb556f4655f638ea8da29061
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-11-23 16:18:41 -0500

    Main

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Project4.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      escript: [main_module: Boss, emu_args: [ "+P 5000000" ],],
       deps: deps()
     ]
   end
