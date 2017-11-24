af8cd30d33ca91bdd23cae4898f8e5b2cbd82478
blackthorn39 <blackthorn_39@live.in>
2017-11-24 14:51:55 -0500

    server skeletal

---
+++ lib/server.ex
@@ -0,0 +1,24 @@
+defmodule Server do
+    use GenServer
+    def start_link({n}) do
+        GenServer.start_link(__MODULE__, {n}, name: String.to_atom("server"))    
+    end
+    def init({n}) do        
+        # state: 
+        # ets tables
+         {:ok, {n}}
+     end
+     def handle_call({:registeruser,x},_,{n}) do
+        #update table (add a new user x)
+        {:reply,"ok",{n}}
+     end
+     def handle_cast({:subscribe,x,subscribe_to},{n})do
+        #update table (add subscribe to for user x)
+        {:noreply,{n}}
+     end
+     def handle_cast({:tweet,x,msg},{n})do
+        #update table (add msg to tweet list of x)
+        #cast message to all subscribers of x
+        {:noreply,{n}}
+     end
+end
