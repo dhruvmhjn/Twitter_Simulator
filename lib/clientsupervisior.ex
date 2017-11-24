696abb7b72a77a3ff8c161435c8b80cd50a9bb4b
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-11-24 15:06:41 -0500

    client sup

---
+++ lib/clientsupervisior.ex
@@ -0,0 +1,15 @@
+defmodule ClientSupervisor do
+    use Supervisor
+    def start_link(clients,time) do
+        {:ok,pid}= Supervisor.start_link(__MODULE__,{nodes,requests},[])
+        
+        #send(Process.whereis(:boss),{:nodes_created})
+        
+        {:ok,pid}
+    end
+    def init({clients,time}) do
+        n_list = Enum.to_list 1..clients
+        children = Enum.map(n_list, fn(x)->worker(Client, [x,clients], [id: "client#{x}"]) end)
+        supervise children, strategy: :one_for_one
+    end
+end
