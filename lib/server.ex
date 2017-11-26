3d0fc97486397781f8d7665b47f89caa631e3578
blackthorn39 <blackthorn_39@live.in>
2017-11-26 14:51:20 -0500

    server state + clientnode

--- lib/server.ex
+++ lib/server.ex
@@ -1,15 +1,15 @@
 defmodule Server do
     use GenServer
     def start_link(n,_,clientnode) do
-        GenServer.start_link(__MODULE__, {n}, name: String.to_atom("server"))    
+        GenServer.start_link(__MODULE__, {n,clientnode}, name: String.to_atom("server"))    
     end
-    def init({n}) do        
+    def init({n,clientnode}) do        
         # state: 
         # ets tables
         :ets.new(:tab_user, [:set, :protected, :named_table])
-         {:ok, {n}}
+         {:ok, {n,clientnode}}
      end
-     def handle_call({:registeruser,x},_,{n}) do
+     def handle_call({:registeruser,x},_,{n,clientnode}) do
         #update table (add a new user x)
         IO.puts("Registering user #{x}")
 
@@ -18,9 +18,9 @@ defmodule Server do
         #IO.inspect res
         [_,_,_,_,_,_,_,{:size, recsize},_,_,_,_,_] = :ets.info(:tab_user)
         IO.inspect recsize
-        {:reply,"ok",{n}}
+        {:reply,"ok",{n,clientnode}}
      end
-     def handle_cast({:subscribe,x,subscribe_to},{n})do
+     def handle_cast({:subscribe,x,subscribe_to},{n,clientnode})do
         #update table (add subscribe to for user x)
         [{_,old_list,_,_}] = :ets.lookup(:tab_user, x)
         subscribe_to = subscribe_to -- [x]
@@ -29,11 +29,11 @@ defmodule Server do
         #update table (add x to followers list)
         res = Enum.map(subscribe_to, fn(y)->:ets.update_element(:tab_user, y, {3, [x]++List.flatten(:ets.match(:tab_user, {y,:"_",:"$1",:"_"}))})end)
         IO.inspect :ets.select(:tab_user, [{{:"$1", :"$2", :"$3",:"$4"}, [], [:"$_"]}])
-        {:noreply,{n}}
+        {:noreply,{n,clientnode}}
      end
-     def handle_cast({:tweet,x,msg},{n})do
+     def handle_cast({:tweet,x,msg},{n,clientnode})do
         #update table (add msg to tweet list of x)
         #cast message to all subscribers of x if ALIVE
-        {:noreply,{n}}
+        {:noreply,{n,clientnode}}
      end
 end
