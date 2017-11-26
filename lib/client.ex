1615f3bb29bbfd7bc2dfbf7ff9881c97051857da
blackthorn39 <blackthorn_39@live.in>
2017-11-26 14:47:38 -0500

    client casts to server node

--- lib/client.ex
+++ lib/client.ex
@@ -1,30 +1,30 @@
 defmodule Client do
     use GenServer
-    def start_link(x,_,servernode) do
+    def start_link(x,clients,servernode) do
         input_srt = Integer.to_string(x)
-        GenServer.start_link(__MODULE__, {x}, name: String.to_atom("user#{x}"))    
+        GenServer.start_link(__MODULE__, {x,servernode}, name: String.to_atom("user#{x}"))    
     end
 
-    def init({x}) do        
+    def init({x,servernode}) do        
        # register self
-        {:ok, {x,10}}
+        {:ok, {x,10,servernode}}
     end
 
-    def handle_cast({:register},{x,acts})do
+    def handle_cast({:register},{x,acts,servernode})do
         #Send register request to server
-        GenServer.call(:server,{:registeruser,x})
+        GenServer.call({:server,,servernode},{:registeruser,x})
         GenServer.cast(:orc, {:registered})
-        {:noreply,{x,acts}}
+        {:noreply,{x,acts,servernode}
     end
-    def handle_cast({:activate, subscribe_to},{x,acts})do
+    def handle_cast({:activate, subscribe_to},{x,acts,servernode})do
         #Subcribe to users
         #IO.puts "Client #{x} asked to activated, sub list = #{subscribe_to}"
-        GenServer.cast(:server,{:subscribe,x,subscribe_to})
+        GenServer.cast({:server,servernode},{:subscribe,x,subscribe_to})
         #Randomly start tweeting/retweeting/subscribe/querying activities acc to zipf rank
         #GenServer.cast(self,{:pick_random,1})
-        {:noreply,{x,acts}}
+        {:noreply,{x,acts,servernode}}
     end
-    def handle_cast({:pick_random,current_state},{x,acts}) do
+    def handle_cast({:pick_random,current_state},{x,acts,servernode}) do
         if(current_state >=  acts) do
         
         else
@@ -45,23 +45,23 @@ defmodule Client do
             end
             GenServer.cast(self(),{:pick_random,current_state + 1})
         end
-        {:noreply,{x,acts}}  
+        {:noreply,{x,acts,servernode}}  
     end
-    def handle_cast({:deactivate},{x,acts})do
+    def handle_cast({:deactivate},{x,acts,servernode})do
         #stop all activities, play dead
         #inform server
-        {:noreply,{x,acts}}
+        {:noreply,{x,acts,servernode}}
     end
 
     def tweet(x) do
         #Generate a message
         msg = "160 random characters"
-        GenServer.cast(:server,{:tweet,x,msg})
+        GenServer.cast({:server,servernode},{:tweet,x,msg})
     end
     def subscribe(x) do
         #Pick random user
         subscribe_to = "user1"
-        GenServer.cast(:server,{:subscribe,x,subscribe_to})
+        GenServer.cast({:server,servernode},{:subscribe,x,subscribe_to})
     end
     def query() do
         
