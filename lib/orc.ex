7fd9faab2e1dcd5193fa659ac8c669368042d9d2
dhruvmhjn <dhruvmhjn@gmail.com>
2017-11-26 20:53:59 -0500

    Orc

--- lib/orc.ex
+++ lib/orc.ex
@@ -14,7 +14,7 @@ defmodule Orc do
 
         nodeid_list = Enum.map(n_list, fn(x) -> "user"<>Integer.to_string(x) end)
         Enum.map(nodeid_list, fn(x) -> GenServer.cast(String.to_atom(x),{:register}) end)
-        {:noreply,{numClients,timePeriod,numRegistered}}
+        {:noreply,{numClients,timePeriod,numRegistered,numCompleted}}
      end
 
     def handle_cast({:registered},{numClients,timePeriod,numRegistered,numCompleted})do
@@ -23,14 +23,14 @@ defmodule Orc do
             IO.puts "Finished registration"
             GenServer.cast(:orc,{:begin_activate})
         end
-        {:noreply,{numClients,timePeriod,numRegistered}}
+        {:noreply,{numClients,timePeriod,numRegistered,numCompleted}}
     end
-    
+
     def handle_cast({:begin_activate},{numClients,timePeriod,numRegistered,numCompleted})do
         n_list = Enum.to_list 1..numClients
         sub_list = Enum.map(1..numClients, fn(_)-> Enum.map(Range.new(1,:rand.uniform(10)), fn(_)->:rand.uniform(numClients)end)end)
         Enum.map(n_list, fn(x) -> GenServer.cast(String.to_atom("user"<>Integer.to_string(x)),{:activate, Enum.uniq(Enum.at(sub_list,x-1))}) end)
-        {:noreply,{numClients,timePeriod,numRegistered}}
+        {:noreply,{numClients,timePeriod,numRegistered,numCompleted}}
     end
 
     def handle_cast({:acts_completed},{numClients,timePeriod,numRegistered,numCompleted}) do
