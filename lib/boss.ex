5b3b9caaa652fea6ae4e847ee3014850948a9291
dhruvmhjn <dhruvmhjn@gmail.com>
2017-11-26 20:37:19 -0500

    Io

--- lib/boss.ex
+++ lib/boss.ex
@@ -15,6 +15,7 @@ defmodule Boss do
 
         if Regex.match?(cregex,argstr) do
             #Client Simulator
+            
             [numClients,timePeriod,serverip]=String.split(argstr," ")
             numClientsInt = String.to_integer(numClients)
             timePeriodInt = String.to_integer(timePeriod)
@@ -23,16 +24,25 @@ defmodule Boss do
             Node.set_cookie :dmahajan
             :global.register_name(:client_boss, self())
             servernode = String.to_atom("servernode@"<>serverip)
-            abc = Node.connect(servernode)
+            connect_result = Node.connect(servernode)
             :global.sync()
-            ClientSupervisor.start_link([numClientsInt,timePeriodInt,servernode]) 
+            if (connect_result == true) do
+                IO.puts "Successfully connected to server at #{serverip}."
+                IO.puts "Starting twitter simulation with #{numClients} users on node #{snode}"
+                IO.puts "The Activity level of each user is a multiple (ZIPF) of the Minimum activities entered."
+                ClientSupervisor.start_link([numClientsInt,timePeriodInt,servernode]) 
+            else
+                IO.puts "Can't connect to server at #{serverip} , try again."
+            end
+            GenServer.call({:server,servernode},{:simulator_add,snode})
         else
             #Server Twitter Engine
             snode=String.to_atom("servernode@"<>ipofsnode)
             Node.start snode
             Node.set_cookie :dmahajan
             :global.register_name(:server_boss, self())
-            ApplicationSupervisor.start_link([String.to_atom("clientnode@"<>"192.168.0.12")]) 
+            IO.puts "Starting twitter engine on node #{snode}"
+            ApplicationSupervisor.start_link([String.to_atom("nonames@"<>"noonodess")]) 
         end
 
         # numClientsInt = String.to_integer(numClients)
