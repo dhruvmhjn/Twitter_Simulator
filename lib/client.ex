44f555e44ce5116b47e0f68e56f6e4a5f9037b7d
dhruvmhjn <dhruvmhjn@gmail.com>
2017-11-30 14:01:22 -0500

    Fis

--- lib/client.ex
+++ lib/client.ex
@@ -7,16 +7,17 @@ defmodule Client do
 
     def init({x,acts,servernode,clients}) do        
        # register self
-        {:ok, {x,acts,servernode,clients}}
+       tweets_pool = ["160 characters from user #{x}","COP5063 is a good course","last sample tweet","one more tweer","One more", "And one more"]
+        {:ok, {x,acts,servernode,clients,tweets_pool}}
     end
 
-    def handle_cast({:register},{x,acts,servernode,clients})do
+    def handle_cast({:register},{x,acts,servernode,clients,tweets_pool})do
         #Send register request to server
         GenServer.call({:server,servernode},{:registeruser,x})
         GenServer.cast(:orc, {:registered})
-        {:noreply,{x,acts,servernode,clients}}
+        {:noreply,{x,acts,servernode,clients,tweets_pool}}
     end
-    def handle_cast({:activate, subscribe_to},{x,acts,servernode,clients})do
+    def handle_cast({:activate, subscribe_to},{x,acts,servernode,clients,tweets_pool})do
         #Subcribe to users
         #IO.puts "Client #{x} asked to activated, sub list = #{subscribe_to}"
         GenServer.cast({:server,servernode},{:subscribe,x,subscribe_to})
@@ -30,77 +31,74 @@ defmodule Client do
             x <= (clients*0.1) ->
                 acts * 10
             
-            x <= (clients*0.7) ->
-                acts * 5
+            x <= (clients*0.6) ->
+                acts * 2
 
             true ->
                 acts
 
         end
 
-        # if (x < (clients*0.1)) do
-        #     acts = acts * 10
-        # end
-        # if (x < (clients*.01) do
-        #     acts = acts * 100
-        # end
-
-        # if 
-
         acts = acts * length(subscribe_to)
         
         GenServer.cast(self,{:pick_random,1})
-        {:noreply,{x,acts,servernode,clients}}
+        {:noreply,{x,acts,servernode,clients,tweets_pool}}
     end
 
-    def handle_cast({:pick_random,current_state},{x,acts,servernode,clients}) do
+    def handle_cast({:pick_random,current_state},{x,acts,servernode,clients,tweets_pool}) do
         if(current_state < acts) do
-            choice = rem(round(:rand.uniform()*100000),5)
+            choice = rem(:rand.uniform(999999),10)
             case choice do
                 1 ->   
                     #subscribe(x,servernode,clients)
-                    tweet(x,servernode)  
+                    tweet_hash(x,servernode,tweets_pool,clients)  
 
                 2 -> 
-                    tweet(x,servernode)
-
-                3 ->
-                    queryhashtags(x,servernode)
-
-                4 ->
-                    querymentions(x,clients,servernode)
+                    tweet_mention(x,servernode,tweets_pool,clients)
 
                 _ ->
+                    tweet(x,servernode,tweets_pool)
                     #querytweets(x)
-
 
             end
             GenServer.cast(self(),{:pick_random,current_state + 1})
         else
             GenServer.cast(:orc, {:acts_completed})
         end
-        {:noreply,{x,acts,servernode,clients}}  
+        {:noreply,{x,acts,servernode,clients,tweets_pool}}  
     end
-    def handle_cast({:deactivate},{x,acts,servernode,clients})do
+    def handle_cast({:deactivate},{x,acts,servernode,clients,tweets_pool})do
         #stop all activities, play dead
         #inform server
         {:noreply,{x,acts,servernode,clients}}
     end
-    def handle_cast({:incoming_tweet,source,msg},{x,acts,servernode,clients})do
-        IO.puts "user#{x} received a tweet from user#{source}:: #{msg}"
+    def handle_cast({:incoming_tweet,source,msg},{x,acts,servernode,clients,tweets_pool})do
+        #IO.puts "user#{x} received a tweet from user#{source}:: #{msg}"
         {:noreply,{x,acts,servernode,clients}}
     end
 
-    def handle_cast({:query_result,result},{x,acts,servernode,clients})do
-        IO.puts "user #{x} received result of query:: #{result}"
-        {:noreply,{x,acts,servernode,clients}}
+    def handle_cast({:query_result,result},{x,acts,servernode,clients,tweets_pool})do
+        #IO.puts "user #{x} received result of query:: #{result}"
+        {:noreply,{x,acts,servernode,clients,tweets_pool}}
     end
 
-    def tweet(x,servernode) do
+    def tweet(x,servernode,tweets_pool) do
         #Generate a message
-        msg = "160 random characters"
+        msg = Enum.random(tweets_pool)
         GenServer.cast({:server,servernode},{:tweet,x,msg})
     end
+
+    def tweet_hash(x,servernode,tweets_pool,clients) do
+        #Generate a message
+        msg = Enum.random(tweets_pool) <> "#hashtag" <>Integer.to_string(:rand.uniform(999))
+        GenServer.cast({:server,servernode},{:tweet,x,msg})
+    end
+
+    def tweet_mention(x,servernode,tweets_pool,clients) do
+        msg = Enum.random(tweets_pool) <> "@user"<>Integer.to_string(:rand.uniform(clients))
+        GenServer.cast({:server,servernode},{:tweet,x,msg})
+    end
+
     def subscribe(x,servernode,clients) do
         #Pick random user
         follow = :rand.uniform(clients)
@@ -113,9 +111,9 @@ defmodule Client do
         hashtag = "#twitter"
         GenServer.cast({:server,servernode},{:hashtags,x,hashtag})
     end
-    def querymentions(x,clients,servernode) do
+    def query_self_mentions(x,clients,servernode) do
         #Pick a random user
-        mention = "@user"<>Integer.to_string(:rand.uniform(clients))
+        mention = "@user"<>Integer.to_string(x)
         GenServer.cast({:server,servernode},{:mentions,x,mention})
     end
     
