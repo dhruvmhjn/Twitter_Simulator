efa9f921b556e3dcb1b3c6ff62dbe28aaf1cb8a4
dhruvmhjn <dhruvmhjn@gmail.com>
2017-11-30 18:32:03 -0500

    Wargning lceanup

--- lib/client.ex
+++ lib/client.ex
@@ -1,14 +1,28 @@
 defmodule Client do
     use GenServer
     def start_link(x,clients,servernode,acts) do
-        input_srt = Integer.to_string(x)
+        #input_srt = Integer.to_string(x)
         GenServer.start_link(__MODULE__, {x,acts,servernode,clients}, name: String.to_atom("user#{x}"))    
     end
 
     def init({x,acts,servernode,clients}) do        
-       # register self
+       # Register self
        tweets_pool = ["160 characters from user #{x}","COP5063 is a good course","last sample tweet","one more tweer","One more", "And one more"]
-        {:ok, {x,acts,servernode,clients,tweets_pool}}
+       #ZIPF: Randomly start tweeting/retweeting/subscribe/querying activities acc to zipf rank
+       acts = cond do
+            x <= (clients*0.01) ->
+                acts * 100
+                
+            x <= (clients*0.1) ->
+                acts * 10
+            
+            x <= (clients*0.6) ->
+                acts * 2
+
+            true ->
+                acts
+        end
+       {:ok, {x,acts,servernode,clients,tweets_pool}}
     end
 
     def handle_cast({:register},{x,acts,servernode,clients,tweets_pool})do
@@ -21,27 +35,8 @@ defmodule Client do
         #Subcribe to users
         #IO.puts "Client #{x} asked to activated, sub list = #{subscribe_to}"
         GenServer.cast({:server,servernode},{:subscribe,x,subscribe_to})
-        
-        #ZIPF: Randomly start tweeting/retweeting/subscribe/querying activities acc to zipf rank
-
-        acts = cond do
-            x <= (clients*0.01) ->
-                acts * 100
-                
-            x <= (clients*0.1) ->
-                acts * 10
-            
-            x <= (clients*0.6) ->
-                acts * 2
-
-            true ->
-                acts
-
-        end
-        
-        #acts = acts * length(subscribe_to)
-        
-        GenServer.cast(self,{:pick_random,1})
+        #START TWEETING
+        GenServer.cast(self(),{:pick_random,1})
         {:noreply,{x,acts,servernode,clients,tweets_pool}}
     end
 
@@ -81,6 +76,7 @@ defmodule Client do
             else
                 "RT, Source: #{source}" <> msg
             end
+            IO.puts "Retweeting: "<>rt_msg
             GenServer.cast({:server,servernode},{:tweet,x,rt_msg})
         end
         {:noreply,{x,acts,servernode,clients,tweets_pool}}
@@ -97,7 +93,7 @@ defmodule Client do
         GenServer.cast({:server,servernode},{:tweet,x,msg})
     end
 
-    def tweet_hash(x,servernode,tweets_pool,clients) do
+    def tweet_hash(x,servernode,tweets_pool,_) do
         #Generate a message
         msg = Enum.random(tweets_pool) <> "#hashtag" <>Integer.to_string(:rand.uniform(999))
         GenServer.cast({:server,servernode},{:tweet,x,msg})
@@ -120,7 +116,7 @@ defmodule Client do
         hashtag = "#twitter"
         GenServer.cast({:server,servernode},{:hashtags,x,hashtag})
     end
-    def query_self_mentions(x,clients,servernode) do
+    def query_self_mentions(x,_,servernode) do
         #Pick a random user
         mention = "@user"<>Integer.to_string(x)
         GenServer.cast({:server,servernode},{:mentions,x,mention})
